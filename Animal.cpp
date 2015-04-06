ad1cb41025889b5979e052fcc3fd65c55e852534
The-Collector <thecollector989@gmail.com>
2015-04-06 08:34:12 -0700

    class done

--- Animal.cpp
+++ Animal.cpp
@@ -1,1 +1,83 @@
+/**********************************************************
+* AUTHOR			: TC & Druox 2.0
+* STUDENT ID		: 366084 & 1010141
+* Lab 12			: Intro to OOP
+* CLASS 			: CS1B
+* SECTION 			: M W 8:30a - 12:20p
+* DUE DATE 			: 4/6/15
+**********************************************************/
+#include "Animal.h"
+
+Animal::Animal ()
+{
+    age           = 0;
+    value         = 0;
+    isInitialized = false;
+
+    name.clear();
+    animal.clear();
+}
+
+Animal::~Animal ()
+{
+    age           = 0;
+    value         = 0;
+    isInitialized = false;
+
+    name.clear();
+    animal.clear();
+}
+
+void Animal::DisplayAnimal()
+{
+    const int NAME_WID  = 19;
+    const int PRECISION = 2;
+    const int VAL_WID   = 13;
+
+    cout << left << fixed    << setprecision(PRECISION);
+    cout << setw(ANIMAL_WID) << animal << setw(NAME_WID)
+         << name  << age     << right  << setw(VAL_WID)
+         << value << endl;
+}
+
+void Animal::ChangeValue(float newValue)
+{
+    value = newValue;
+}
+
+void Animal::ChangeAge(int newAge)
+{
+    age = newAge;
+}
+
+void Animal::ChangeName(string newName)
+{
+    name = newName;
+}
+
+void Animal::AnimalType(string type)
+{
+    animal = type;
+}
+
+bool Animal::IsInitialized()
+{
+    return isInitialized;
+}
+
+void Animal::InitializeAll(int nAge, float nValue, string nName, string nType)
+{
+    age    		  = nAge;
+    value  	  	  = nValue;
+    name     	  = nName;
+    animal		  = nType;
+    isInitialized = true;
+
+
+}
+
+void Animal::DisplayName()
+{
+    cout << name;
+}
 
--- Animal.h
+++ Animal.h
@@ -1,1 +1,40 @@
+/**********************************************************
+* AUTHOR			: TC & Druox 2.0
+* STUDENT ID		: 366084 & 1010141
+* Lab 12			: Intro to OOP
+* CLASS 			: CS1B
+* SECTION 			: M W 8:30a - 12:20p
+* DUE DATE 			: 4/6/15
+**********************************************************/
+#ifndef ANIMAL_H_
+#define ANIMAL_H_
+#include "header.h"
+class Animal
+{
+  public:
+    Animal ();
+    ~Animal ();
 
+    void InitializeAll(int nAge, float nValue, string nName, string nType);
+    void ChangeAge(int newAge);
+    void ChangeValue(float newValue);
+    void DisplayAnimal();
+    void ChangeName(string newName);
+    void AnimalType(string type);
+    bool IsInitialized();
+    void DisplayName();
+
+  private:
+    int    age;
+    double value;
+    string name;
+    string animal;
+    bool   isInitialized;
+};
+
+void Initialize(Animal &fluffy, Animal &maa, Animal &babe);
+void AgeChange(Animal &fluffy, Animal &maa, Animal &babe);
+void DisplayAllAnimals(Animal fluffy, Animal maa, Animal babe);
+void ValueChange(Animal &fluffy, Animal &maa, Animal &babe);
+
+#endif /* ANIMAL_H_ */
